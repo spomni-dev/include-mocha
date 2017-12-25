47c35c5b4ce5f886c25817527c82e12e71872e40
Spomni <mikhail.snitko@gmail.com>
2017-12-25 20:42:54 +0300

    #0-3-0 Create a test plan

--- test/spec/include-mocha-spec.js
+++ test/spec/include-mocha-spec.js
@@ -29,3 +29,190 @@
  * @method {HTMLLinkElement|Error} includeStylesheet - Include in document a stylesheet file.
  *
  */
+
+/** @function includeMocha.includeScript()
+ *
+ * @description Insert a script node into the head node.
+ *
+ * @param {string} src
+ * @param {boolean} async
+ *
+ * @returns {HTMLScriptElement|Error} - Object of the class Error or the inseted node.
+ *
+*/
+
+/* include-mocha.js */
+  //
+  /* include-mocha.js loaded */
+    //
+    /* includeMocha */
+      //-- Should contain an object of the class "includeMocha".
+    //
+    /* includeMocha.option */
+      //-- Should contain null.
+    //
+    /* includeMocha.runner */
+      //-- Should contain null.
+    //
+    /* includeMocha.includeScript( src, async ) */
+      //
+      //-- Should contain an object of the class "Function"
+      //
+      //-- Should return an object of class "Error" if the param "src" is not string or undefined.
+      //-- Should return an object of class "Error" if the param "async" is not boolean or undefined.
+      //
+      //-- Should insert a script node into the node "head" as last child.
+      //-- The attribute "src" of the inserted node should be equal to the param "src".
+      //-- The attribute "async" of the inserted node should be equal to the param "async" if param "async" is defined.
+      //-- The attribute "async" of the inserted node should be false if the param "async" is undefined.
+      //
+      //-- Should return an object of the HTMLScriptElement inserted in document.
+    //
+    /* includeMocha.includeStylesheet( href ) */
+      //
+      //-- Should return an object of the class "Error" if the param "href" is not string.
+      //
+      //-- Shoule insert a node "link" into the node "head" as last child.
+      //-- The attribute "href" of the inserted node should be equal to the param "href".
+      //
+      //-- Should return an object of the class "HTMLLinkElement" inserted in document.
+    //
+  //
+  /* includeMocha( option ) */
+    //
+    /* Check input params */
+      //
+      /* Check type of the param "option" */
+        //-- Should return an object of the class "Error" if the param "option" is not string or array or object of the class "Object".
+      //
+      /* If the param "option" is array */
+        //-- Should return an object of the class "Error" if any element of the array "option" is not string.
+      //
+      /* If the param "option" is object */
+        //
+        //-- Should return an object of the class "Error" if the param "option.specPath" is not string or array.
+        //-- Should return an object of the class "Error" if the param "option.specPath" is array and any its element is not string.
+        //-- Should return an object of the class "Error" if the param "option.specRoot" is not string or null or undefined.
+        //
+        //-- Should return an object of the class "Error" if the param "option.cssPath" is not string or array or null or undefined.
+        //-- Should return an object of the class "Error" if the param "option.specPath" is array and any its alement is not string.
+        //-- Should return an object of the class "Error" if the param "option.specRoot" is not string or null or undefined.
+        //
+        //-- Should return an object of the class "Error" if the param "option.useChai" is not boolean or null or undefined.
+        //-- Should return an object of the class "Error" if the param "option.defineAssert" is not boolean or null or undefined.
+        //
+        //-- Should return an object of the class "Error" if the param "option.mochaPath" is not string or undefined.
+        //-- Should return an object of the class "Error" if the param "option.useChai" is true and the param "option.chaiPath" is not string or undefined.
+        //-- Should return an object of the class "Error" if the param "option.libRoot" is not string or undefined.
+        //
+        //-- Should return an object of the class "Error" if the param "option.mochaSetup" is not string or an object of the class "Object".
+      //
+    //
+    /* Check global vars before init */
+      //
+      //-- Should return an object of the class "Error" if the global variable "mocha" is defined.
+      //-- Should return an object of the class "Error" if the global variable "chai" is defined and the param "useChai" is true.
+      //-- Should return an object of the class "Error" if the global variable  "assert" is defined and the params "useChai" and "defineAssert" are true.
+    //
+    /* Check init of the member "includeMocha.option" */
+      //
+      /* includeMocha.option.specPath */
+        //
+        //-- Should be array.
+        //-- Should contain one element equal to the param "option.specPath" if it is string.
+        //-- Should contain all elements of the param "option.specPath" if it is array.
+      //
+      /* includeMocha.option.specRoot */
+        //
+        //-- Should be string or null.
+        //-- Should be equal to "spec/" if the param "option.specRoot" is undefined.
+        //-- Should be equal to the param "option.specRoot" if it is defined.
+      //
+      /* includeMocha.option.cssPath */
+        //
+        //-- Should be array or null
+        //-- Should contain one element "mocha.js" if the param "option.cssPath" is undefined.
+        //-- Should be equal to the param "option.cssPath" if it is defined.
+      //
+      /* includeMocha.option.cssRoot */
+        //
+        //-- Should be string or null
+        //-- Should be equal to "css/" if the param "option.cssRoot" is undefined.
+        //-- Should be equal to the param "option.cssRoot" if ot is defined.
+      //
+      /* includeMocha.option.useChai */
+        //
+        //-- Should be boolean
+        //-- Should be equal to true if the param "option.useChai" is undefined.
+        //-- Should be equal to the param "option.useChai" if it is defined.
+      //
+      /* includeMocha.option.defineAssert */
+        //
+        //-- Should be boolean
+        //-- Should be equal to true if the param "option.defineAssert" is undefined.
+        //-- Should be equal to the param "option.defineAssert" if it is defined.
+      //
+      /* includeMocha.option.mochaPath */
+        //
+        //-- Should be string
+        //-- Should be equal to "mocha.js" if the param "option.mochaPath" is undefined.
+        //-- Should be equal to the param "option.mochaPath" if it is defined.
+      //
+      /* includeMocha.option.chaiPath */
+        //
+        //-- Should be string
+        //-- Should be equal to "chai.js" if the param "option.chaiPath" is undefined.
+        //-- Should be equal to the param "option.chaiPath" if it is defined.
+      //
+      /* includeMocha.option.libRoot */
+        //-- Should be string or null
+        //-- Should be equal to "lib/" if the param "option.libRoot" is undefined.
+        //-- Should be equal to the param "option.libRoot" if it is defined.
+      //
+      /* includeMocha.option.mochaSetup */
+        //
+        //-- Should be string or object of the class "Object".
+        //-- Should be equal to "bdd" if the param "option.mochaSetup" is undefined.
+        //-- Should be equal to the param "option.mochaSetup" if it is defined.
+      //
+    //
+    /* Check the inclusion of css and lib files */
+      //
+      /* Check the inclusion stylesheets */
+        //
+        //-- The DON-node "head" should contain the DOM-node "link" for each element of the array "includeMocha.option.cssPath" for it is not null.
+        //-- The attribute "href" of each inserted node should be equal to the string (includeMocha.option.cssRoot + includeMocha.option.cssPath[i]) if includeMocha.option.cssPath is not null.
+      //
+      /* Check the inclusion of lib files */
+        //
+        //-- The DON-node "head" should contain the DOM-node "script" that has the attribute "src" equal to the string (includeMocha.option.libRoot + includeMocha.option.mochaPath).
+        //-- The DON-node "head" should contain the DOM-node "script" that has the attribute "src" equal to the string (includeMocha.option.libRoot + includeMocha.option.chaiPath) if the includeMocha.option.useChai is true.
+      //
+    //
+    /* Check mocha.setup() errors */
+      //
+      // Should return an object of the class "Error" if the function "mocha.setup()" threw error.
+    //
+    /* Check the inclusion of spec files */
+      //
+      //-- The DON-node "head" should contain the DOM-node "script" for each element of the array "includeMocha.option.cssPath".
+      //-- The attribute "src" of each inserted node should be equal to the string (includeMocha.option.cssRoot + includeMocha.option.specPath[i]).
+    //
+    /* Check mocha.run() errors */
+      //
+      // Should return an object of the class "Error" if the function "mocha.run()" threw error.
+    //
+    /* Check global vars after includeMocha */
+      //
+      //-- The global var "mocha" should be function.
+      //-- The member "name" of the variable "mocha" should be equal to "Mocha".
+      //
+      //-- The global var "chai" should contain an object of the class "Object" if "includeMocha.option.useChai" is true.
+      //-- The global var "chai" should has the defined members "Assertion" and "AssertionError" if "includeMocha.option.useChai" is true.
+      //
+      //-- The global var "assert" should be function if "includeMocha.option.useChai" and "includeMocha.option.defineAssert" are true.
+      //-- The global var "aseert" should has the defined members "Throw" and "changes" if "includeMocha.option.useChai" and "includeMocha.option.defineAssert" are true.
+    //
+  //
+//
+
